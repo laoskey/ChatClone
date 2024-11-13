37452d0f4363aa63e3c5ee76b0c83c075a91e6e1
观澜听雨 <tangxiaochuane@yeah.net>
2024-11-13 15:57:00 +0800

    implement the serversetting page

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,4 +1,23 @@
+import InitialModal from "@/components/modals/InitialModal";
+import { db } from "@/lib/db";
+import { initialProfiles } from "@/lib/initial-profiles";
+import { redirect } from "next/navigation";
+
 interface SetupPageProps {}
 export default async function SetupPage() {
-  return <div>Create a server</div>;
+  const profile = await initialProfiles();
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+  return <InitialModal />;
 }
