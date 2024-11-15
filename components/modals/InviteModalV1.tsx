5e634eda8190d35ec5b8d256a165a63673124a30
观澜听雨 <tangxiaochuane@yeah.net>
2024-11-15 18:12:43 +0800

    implement the invitemodal UI

--- components/modals/InviteModalV1.tsx
+++ components/modals/InviteModalV1.tsx
@@ -6,6 +6,10 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { useModal } from "@/lib/hooks/useModalStore";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Check, Copy, RefreshCw } from "lucide-react";
 
 function InviteModalV1() {
   const { isOpen, onClose, type } = useModal();
@@ -22,7 +26,39 @@ function InviteModalV1() {
             Invite friends
           </DialogTitle>
         </DialogHeader>
-        Invite Modal
+        <div className='p-6'>
+          <Label className='uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70'>
+            Server invite link
+          </Label>
+          <div className='flex items-center mt-2 gap-x-2'>
+            <Input
+              // disabled={isLoading}
+              className='bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0'
+              // value={inviteUrl}
+            />
+            <Button
+              // disabled={isLoading}
+              // onClick={onCopy}
+              size='icon'
+            >
+              {/* {copied ? (
+                <Check className='w-4 h-4' />
+              ) : (
+                <Copy className='w-4 h-4' />
+              )} */}
+            </Button>
+          </div>
+          <Button
+            // onClick={onNew}
+            // disabled={isLoading}
+            variant='link'
+            size='sm'
+            className='text-xs text-zinc-500 mt-4'
+          >
+            Generate a new link
+            <RefreshCw className='w-4 h-4 ml-2' />
+          </Button>
+        </div>
       </DialogContent>
     </Dialog>
   );
