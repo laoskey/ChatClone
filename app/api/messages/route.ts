a8a97dd502d49761cc319025cf0981d41249cd01
观澜听雨 <tangxiaochuane@yeah.net>
2024-11-23 13:38:04 +0800

    implement the find message API

---
+++ app/api/messages/route.ts
@@ -0,0 +1,78 @@
+import { currentProfile } from "@/lib/current-profile";
+import { NextResponse } from "next/server";
+import { Message } from "@prisma/client";
+import { db } from "@/lib/db";
+
+const MESSAGE_BATCH = 10;
+
+export default async function GET(req: Request) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+
+    const cursor = searchParams.get("cursor");
+    const channelId = searchParams.get("channelId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!channelId) {
+      return new NextResponse("Channel ID missing", { status: 400 });
+    }
+
+    let messages: Message[] = [];
+
+    if (cursor) {
+      messages = await db.message.findMany({
+        where: {
+          channelId,
+        },
+        take: MESSAGE_BATCH,
+        skip: 1,
+        cursor: {
+          id: cursor,
+        },
+        include: {
+          member: {
+            include: { profile: true },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    } else {
+      messages = await db.message.findMany({
+        where: {
+          channelId,
+        },
+        take: MESSAGE_BATCH,
+        include: {
+          member: {
+            include: {
+              profile: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+    }
+
+    let nextCursor = null;
+
+    if (messages.length === MESSAGE_BATCH) {
+      nextCursor = messages[MESSAGE_BATCH - 1].id;
+    }
+
+    return NextResponse.json({
+      items: messages,
+      nextCursor,
+    });
+  } catch (error) {
+    console.log("[MESSAGES_GET]", error);
+    return new NextResponse("Interal error", { status: 500 });
+  }
+}
--- components/chat/ChatMessages.tsx
+++ components/chat/ChatMessages.tsx
@@ -3,6 +3,7 @@
 import { Member } from "@prisma/client";
 import ChatWelecome from "@/components/chat/ChatWelecome";
 import useChatQuery from "@/lib/hooks/useChatQuery";
+import { Loader2, ServerCrash } from "lucide-react";
 
 interface ChatMessagesProps {
   name: string;
@@ -35,6 +36,27 @@ function ChatMessages({
     status,
   } = useChatQuery({ queryKey, paramKey, apiUrl, paramValue });
 
+  if (status === "pending") {
+    return (
+      <div className='flex flex-col flex-1 justify-center items-center'>
+        <Loader2 className='h-7 w-7 text-zinc-500 animate-spin my-4' />
+        <p className='text-xs text-zinc-500 dark:text-zinc-400'>
+          Loading messages...
+        </p>
+      </div>
+    );
+  }
+
+  if (status === "error") {
+    return (
+      <div className='flex flex-col flex-1 justify-center items-center'>
+        <ServerCrash className='h-7 w-7 text-zinc-500 my-4' />
+        <p className='text-xs text-zinc-500 dark:text-zinc-400'>
+          Something went wrong!
+        </p>
+      </div>
+    );
+  }
   return (
     <div className='flex-1 flex flex-col py-4 overflow-y-auto'>
       <ChatWelecome
