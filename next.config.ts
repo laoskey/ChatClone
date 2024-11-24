8f667e2852958fea997c74ac21f3ca84390da67d
观澜听雨 <tangxiaochuane@yeah.net>
2024-11-24 13:31:41 +0800

    modify the next-config-ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import { buffer } from "stream/consumers";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+    });
+
+    return config;
+  },
 
   images: {
     domains: ["owaohzxfnovawbdkaobd.supabase.co"],
